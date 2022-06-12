5c6d00ba902eaba6242169ec8197f022ea1dfacc
Arotein <9cksqhdel3@naver.com>
2022-06-13 04:24:33 +0900

    create project

---
+++ arotein/src/main/java/study/arotein/AroteinApplication.java
@@ -0,0 +1,13 @@
+package study.arotein;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AroteinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AroteinApplication.class, args);
+	}
+
+}
---
+++ arotein/src/main/java/study/arotein/HelloController.java
@@ -0,0 +1,13 @@
+package study.arotein;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class HelloController {
+    @RequestMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+}
